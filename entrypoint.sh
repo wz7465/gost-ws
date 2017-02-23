c306ce48da1113d86ebdc1a09b73866968a1ab11
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-02-23 09:54:14 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@ cd /go/bin
 if [ "$CERT_PEM" != "$KEY_PEM" ]; then
   echo "$CERT_PEM" |  tee cert.pem
   echo "$KEY_PEM"  |  tee key.pem
-  ./gost -L $MODE://:8080?cert=./cert.pem&key=./key.pem -logtostderr -v=3
+  ./gost -L $MODE://:8080?cert=./cert.pem&key=./key.pem -logtostderr -v 3
 else
-./gost -L $MODE://:8080 -logtostderr -v=3
+  ./gost -L $MODE://:8080 -logtostderr -v 3
 fi
